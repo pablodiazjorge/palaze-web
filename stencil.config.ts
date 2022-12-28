ffb6b126dcdc1810ed2609040e9c483fe871cc8c
Pablo Díaz-Jorge <pablodiazjorgegarcia@gmail.com>
2022-12-28 13:12:52 +0100

    Modificaciones estructurales básicas

--- stencil.config.ts
+++ stencil.config.ts
@@ -2,6 +2,7 @@ import { Config } from '@stencil/core';
 
 export const config: Config = {
   namespace: 'palaze-web',
+  globalStyle: 'src/global/global-styles.css',
   outputTargets: [
     {
       type: 'dist',
