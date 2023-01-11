cf690038308fc3112758e4472cbc1bec88fafabc
Pablo Díaz-Jorge <pablodiazjorgegarcia@gmail.com>
2023-01-11 18:43:16 +0100

    Minor modifications button component.

--- src/components/plz-button/plz-button.tsx
+++ src/components/plz-button/plz-button.tsx
@@ -7,7 +7,7 @@ import { Component, h, Host, Prop } from '@stencil/core';
 })
 export class PlzButton {
 
-  @Prop() variant?: string = "default-color";
+  @Prop() variant?: string = "default";
   @Prop() size?: string = "default-size"
   @Prop() corners?: string = "default-border";
 
