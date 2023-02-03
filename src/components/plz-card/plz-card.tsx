da9fe234cbe306f2fd5fa49142ba21959462057b
DESKTOP-642QPGV\Pablo Díaz-Jorge <pablodiazjorgegarcia@gmail.com>
2023-02-03 19:39:47 +0100

    Almost finished landing page. Implemented a new card.

--- src/components/plz-card/plz-card.tsx
+++ src/components/plz-card/plz-card.tsx
@@ -8,17 +8,15 @@ import { Component, h, Prop } from '@stencil/core';
 export class PlzCard {
   @Prop() variant: string = 'default';
   @Prop() url: string = '#';
-  @Prop() image: string = './assets/icon/pattern.png';
+  @Prop() image: string = '';
+  @Prop() hover: string = 'blue';
 
   render() {
     return (
-      <a href={this.url}>
+      <a class={`a-${this.variant + ' ' + 'glow-' + this.hover}`} href={this.url}>
         <div class={`card-${this.variant}`}>
-          <div class={`image-content-${this.variant}`}>
-            <span class={`overlay-${this.variant}`} style={{'background-image': `url(${this.image})`}}>
-            </span>
-          </div>
-          <div class={`card-content-${this.variant}`}>
+          <div class={`image-container-${this.variant}`} style={{ 'background-image': `url(${this.image})` }}></div>
+          <div class={`text-container-${this.variant}`}>
             <slot></slot>
           </div>
         </div>
