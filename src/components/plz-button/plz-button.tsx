451ab00ab7d3d19ea5f3f7eeb931ea2b335a4167
DESKTOP-642QPGV\Pablo Díaz-Jorge <pablodiazjorgegarcia@gmail.com>
2023-02-06 16:10:29 +0100

    First modifications, in progres...

--- src/components/plz-button/plz-button.tsx
+++ src/components/plz-button/plz-button.tsx
@@ -6,19 +6,33 @@ import { Component, h, Host, Prop } from '@stencil/core';
   shadow: true,
 })
 export class PlzButton {
+  @Prop() variant?: string = 'default';
+  @Prop() size?: string = 'default';
+  @Prop() color?: string = 'default';
+  @Prop() corners?: string = 'default';
+  @Prop() icon?: string = '';
 
-  @Prop() variant?: string = "default";
-  @Prop() size?: string = "default-size"
-  @Prop() corners?: string = "default-border";
+  ComponentDidLoad() {}
 
   render() {
-    return (
+    return this.variant == 'default' ? (
       <Host>
-        <button class={this.variant + " " + this.size + " " + this.corners}>
+        <button class={this.variant + ' ' + this.color + '-color ' + this.size + '-size ' + this.corners + '-border'}>
           <slot></slot>
         </button>
       </Host>
-    );
+    ) : this.variant == 'icon' ? (
+      <Host>
+        <button class={this.variant + ' ' + this.variant + '-' + this.size + '-size '}>
+          <img class={this.variant + '-' + this.size + '-size'} src={'./assets/icon/Icon-' + `${this.icon}` + '.svg'} alt={`${this.icon}`} />
+        </button>
+      </Host>
+    ) : this.variant == 'progress' ? (
+      <Host>
+        <button class={this.variant + ' ' + this.color + '-color ' + this.size + '-size ' + this.corners + '-border'}>
+          <slot></slot>
+        </button>
+      </Host>
+    ) : null;
   }
-
 }
