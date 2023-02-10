58486e12ebd715b335fdcede5104b88cf504f10d
DESKTOP-642QPGV\Pablo Díaz-Jorge <pablodiazjorgegarcia@gmail.com>
2023-02-10 19:13:01 +0100

    Progressing, new variants and re-organization css class.

--- src/components/plz-card/plz-card.tsx
+++ src/components/plz-card/plz-card.tsx
@@ -12,7 +12,20 @@ export class PlzCard {
   @Prop() hover: string = 'blue';
 
   render() {
-    return (
+    return this.variant == 'default' ? (
+      <a class={`a-${this.variant + ' ' + 'glow-' + this.hover}`} href={this.url}>
+        <div>
+          <div class={`image-container-${this.variant}`} style={{ 'background-image': `url(${this.image})` }}></div>
+          <div class={`text-container-${this.variant}`}>
+            <slot></slot>
+          </div>
+        </div>
+      </a>
+    ) : this.variant == 'simple' ? (
+      <div class={`card-${this.variant}`}>
+          <slot></slot>
+      </div>
+    ) : this.variant == 'header' ? (
       <a class={`a-${this.variant + ' ' + 'glow-' + this.hover}`} href={this.url}>
         <div class={`card-${this.variant}`}>
           <div class={`image-container-${this.variant}`} style={{ 'background-image': `url(${this.image})` }}></div>
@@ -21,6 +34,15 @@ export class PlzCard {
           </div>
         </div>
       </a>
-    );
+    ) : this.variant == 'footer' ? (
+      <a class={`a-${this.variant + ' ' + 'glow-' + this.hover}`} href={this.url}>
+        <div class={`card-${this.variant}`}>
+          <div class={`image-container-${this.variant}`} style={{ 'background-image': `url(${this.image})` }}></div>
+          <div class={`text-container-${this.variant}`}>
+            <slot></slot>
+          </div>
+        </div>
+      </a>
+    ) : null;
   }
 }
